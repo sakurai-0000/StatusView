8f47c24122906c08c569a21c121177876a5f8e9c
antyan6300 <sr.macnica19910603@icloud.com>
2020-02-02 10:47:29 +0900

    Updates

---
+++ precache-manifest.dc3a369d3b0bde667200304d02f61dc3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33f4dd74b3d00b5340f2c3f98da34ae1",
+    "url": "/StatusView/index.html"
+  },
+  {
+    "revision": "f9c134a0da27c3b68b65",
+    "url": "/StatusView/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b3406fbf341006300ca4",
+    "url": "/StatusView/static/js/2.554081d6.chunk.js"
+  },
+  {
+    "revision": "f09321e517ab4567bb350ec8faae3b4d",
+    "url": "/StatusView/static/js/2.554081d6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f9c134a0da27c3b68b65",
+    "url": "/StatusView/static/js/main.e82dcf8d.chunk.js"
+  },
+  {
+    "revision": "5c5660573ef341ba9b84",
+    "url": "/StatusView/static/js/runtime-main.7ac5c93a.js"
+  }
+]);
